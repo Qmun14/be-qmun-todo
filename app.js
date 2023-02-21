44e0d325d3125c11c374e8ef5b9dd0946c1cef9e
Qmun14 <mamunramdhan@gmail.com>
2023-02-21 18:08:31 +0700

    setup expressJS routing dan config server-nya

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const todosRouter = require('./app/api/todos/router')
 
-var app = express();
+const URL = '/api/v1'
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.json({
+        message: 'welcome api qmun-todos',
+        version: '1.0.1'
+    })
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(`${URL}`, todosRouter);
 
 module.exports = app;
